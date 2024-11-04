fce6238ef9b44d3c25ab4078c7801ab07b8b483d
Cyberine101 <bryce.a.green.bg@gmail.com>
2024-11-04 04:13:59 -0500

    Uploaded railroad files.

---
+++ Railroad.java
@@ -0,0 +1,19 @@
+/* Bryce Green
+ * Dr. Steinberg
+ * COP 3503 Spring 2023
+ * Programming Assignment 5
+ */
+
+public class Railroad {
+    private int tracks;
+    private String name;
+    
+    public Railroad(int tracks, String name){
+        this.tracks = tracks;
+        this.name = name;
+    }
+    
+    public String buildRailroad(){
+        
+    }
+}
